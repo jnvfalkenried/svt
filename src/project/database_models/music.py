f2648215720cfcd4ad7f2a8c7c92c8fbfd81fdeb
anandmatt <anandmathewms@gmail.com>
2024-10-13 17:32:32 +0200

    Created DB Schema

---
+++ src/project/database_models/music.py
@@ -0,0 +1,30 @@
+from project.database_models.base import Base
+from typing import Optional
+from sqlalchemy import Integer, String, Boolean, Index
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+
+class Music(Base):
+    __tablename__ = "music"
+    
+    id:Mapped[str] = mapped_column(String, primary_key=True, unique=True)
+    author_name:Mapped[Optional[str]] = mapped_column(String, nullable=True)
+    title:Mapped[Optional[str]] = mapped_column(String, nullable=True)
+    original:Mapped[Optional[bool]] = mapped_column(Boolean, nullable=True)
+    duration:Mapped[Optional[int]] = mapped_column(Integer, nullable=True)
+    
+    posts = relationship("Posts", back_populates="music")
+    
+    __table_args__ = (
+        Index("music_id", "id"),
+        Index("music_author_name", "author_name"),
+    )
+
+    def __repr__(self):
+        return (
+            f"Music("
+            f"id={self.id}, "
+            f"author_name={self.author_name!r}, "
+            f"title={self.title!r}, "
+            f"duration={self.duration!r}, "
+            f")"
+        )
