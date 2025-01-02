4e298a7a47191458727a9be29ca02a96690a280f
anandmatt <anandmathewms@gmail.com>
2025-01-02 13:04:35 +0100

    ISORT compliance

--- src/postgresql/alembic/versions/0a679bf06c73_hashed_id_from_list.py
+++ src/postgresql/alembic/versions/0a679bf06c73_hashed_id_from_list.py
@@ -8,9 +8,8 @@ Create Date: 2024-12-26 19:08:25.321032
 
 from typing import Sequence, Union
 
-from alembic import op
 import sqlalchemy as sa
-
+from alembic import op
 
 # revision identifiers, used by Alembic.
 revision: str = "0a679bf06c73"
--- src/postgresql/alembic/versions/d9eaa08778c7_added_tables_for_extended_hashtags.py
+++ src/postgresql/alembic/versions/d9eaa08778c7_added_tables_for_extended_hashtags.py
@@ -8,9 +8,8 @@ Create Date: 2024-12-05 11:26:36.453381
 
 from typing import Sequence, Union
 
-from alembic import op
 import sqlalchemy as sa
-
+from alembic import op
 
 # revision identifiers, used by Alembic.
 revision: str = "d9eaa08778c7"
